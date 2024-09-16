a7f3a4dffc54cc71f25903475064b51171d15c5e
Tyler Rouze <tyler.rouze@fishtownanalytics.com>
2024-09-16 22:55:21 +0000

    make docs persistence env dependent

--- macros/persist_docs_op.sql
+++ macros/persist_docs_op.sql
@@ -8,14 +8,17 @@
         database=model_node.database, schema=model_node.schema, identifier=model_node.alias
       ) %}
       
-      {% if relation %}
+      {%- set existing_columns = adapter.get_columns_in_relation(relation) -%}
+      {%- set columns_to_persist_docs = adapter.get_persist_doc_columns(existing_columns, model_node.columns) -%}
+
+      {% if relation and not relation.is_view %}
         {{ log("Altering relation comment for " + model_node.alias, info = true) }}
         {{ alter_table_comment(relation, model_node) }}
       {% endif %}
 
-      {% if columns %}
+      {% if columns and not relation.is_view %}
         {{ log("Altering column comments for " + model_node.alias, info = true) }}
-        {{ alter_column_comment(relation, model_node.columns) }}
+        {{ alter_column_comment(relation, columns_to_persist_docs) }}
       {% endif %}
     
     {% endfor %}
